6efe4f912cf1602d6afc2345924f4c58fc7037ed
Rhys Newell <rhys.newell@uqconnect.edu.au>
2019-05-28 21:14:29 +1000

    Adding extra info to setup

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='chipr',
     version='1.0',
+    author='Rhys Newell',
+    suthor_email='r.newell@uq.net.au',
     packages=find_packages(),
     license='GPL-3.0',
     long_description=open('README.md').read(),
@@ -14,5 +16,17 @@ setup(
         ]
     },
     python_requires='>=3',
+    description=('ChIP-R is a method for assessing the reproducibility of ' +
+                 'replicated ChIP-seq type experiments. It incorporates the ' +
+                 'rank product method, a novel thresholding methods, and the ' +
+                 'use of peak fragmentation return the most reproducible peaks.'),
+    keywords='ChIP-R',
+    url='https://github.com/rhysnewell/ChIP-R',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+    ],
 )
 

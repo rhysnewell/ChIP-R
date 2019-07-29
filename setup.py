67df242e534ba157d00b4b1eb540c9b4afaed15d
Rhys Newell <rhys.newell@uqconnect.edu.au>
2019-07-29 20:42:02 +1000

    update version info

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='ChIP-R',
-    version='1.1.1',
+    version='1.1.2',
     author='Rhys Newell, Mikael Boden, Alex Essebier',
     author_email='r.newell@uq.edu.au',
     packages=find_packages(),
